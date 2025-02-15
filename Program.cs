76e1c7847b7a27eea4714241cb9a972bfd33e4fd
yuvan1234 <yuvanbajjurla@gmail.com>
2025-02-15 18:45:49 +0530

    Enable Swagger in production

--- Program.cs
+++ Program.cs
@@ -20,11 +20,12 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "SQLViz API v1");
+    c.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 app.UseCors("AllowAll");  // Enable CORS
